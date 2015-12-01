8bb0baac64e8d93b7c5bfcbe94b19e06ee23df3e
Teoman Soygul <tsoygul@gmail.com>
2015-12-01 11:22:59 +0100

    move seed to init

--- randstr.go
+++ randstr.go
@@ -5,15 +5,19 @@ import (
 	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 // Alphabet is the letters used in creating the random string.
 var Alphabet = []rune(". !abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // Get generates a random string sequence of given size.
 func Get(n int) string {
-	rand.Seed(time.Now().UTC().UnixNano())
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = Alphabet[rand.Intn(len(Alphabet))]
 	}
+
 	return string(b)
 }
